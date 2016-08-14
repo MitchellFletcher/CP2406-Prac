79e3e52962d8f8ed7c41cbe2572f523e772aa475
Mitchell Fletcher <mitchell.fletcher@my.jcu.edu.au>
2016-08-15 00:40:33 +1000

    prac1

---
+++ Prac 1/MovieQuote.java
@@ -0,0 +1,6 @@
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("Go ahead, make my day");
+    }
+}
+
---
+++ Prac 1/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Go ahead, make my day");
+        System.out.println("Harry Callahan");
+        System.out.println("Sudden Impact");
+        System.out.println("1983");
+    }
+}
