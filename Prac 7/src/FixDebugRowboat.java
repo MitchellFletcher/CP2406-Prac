bddfc0b96339fc5a60999fde005a83588b965bec
Mitchell Fletcher <mitchell.fletcher@my.jcu.edu.au>
2016-10-10 08:48:35 +1000

    Pracs!!!!!!

---
+++ Prac 7/src/FixDebugRowboat.java
@@ -0,0 +1,20 @@
+
+public class FixDebugRowboat extends FixDebugBoat
+{
+    public FixDebugRowboat()
+    {
+        super("row");
+        // include setPassengers() and setPower() methods
+        // in class constructor
+        setPassengers();
+        setPower();
+    }
+    public void setPassengers()
+    {
+        super.passengers = 2;
+    }
+    public void setPower()
+    {
+        super.power = "oars";
+    }
+}
